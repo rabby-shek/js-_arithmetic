266fea25fc429a028b365ee47aea5f463cbe550f
rabby-shek <rabbyshek1998@gmail.com>
2022-02-18 19:36:20 +0600

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function calculate() {
+    const result = document.getElementById("result");
+    const principal = Number(document.getElementById("Principal").value);
+    const rate = Number(document.getElementById("Rate").value);
+    result.innerHTML =  (principal * rate) / 100;
+
+}
+function totalInstalment (){
+    const result = document.getElementById("result");
+    const Installment = document.getElementById("instalmentAmount");
+    const TotalAmount = document.getElementById("totalAmount");
+    const principal = Number(document.getElementById("Principal").value);
+    const Total = Number(document.getElementById("total").value);
+    Installment.innerHTML = ( principal + result ) / Total;
+    TotalAmount.innerHTML =  Installment * Total;
+
+
+
+
+
+}
